924b23fd9a40e8dd6733d92e27ce1f47018d3161
Angemarie-coder <126848927+Angemarie-coder@users.noreply.github.com>
2023-06-13 00:16:40 +0200

    Add files via upload

---
+++ 221016875/bill_splitter.py
@@ -0,0 +1,16 @@
+# Name: Ndayishimiye Bonheur
+# reg: 221024068
+# Department: IT
+
+    # Project
+
+
+friends_list = int(input("How many friends will be on dinner?: "))
+friends = {}
+for i in range(friens_list):
+  name = input("Enter the names of friend{}: ".format(i+1))
+  friends[name] = 0
+  
+  friends["You"] = 0
+  print("Friends: ",
+  list(friends.keys()))
